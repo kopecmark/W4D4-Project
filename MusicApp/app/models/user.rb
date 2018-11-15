28b5b95476b06ac2c5045f0b18eecd69868b454d
Mark Kopec <mark.kopec@gmail.com>
2018-11-15 11:37:31 -0800

    Add reset_session_token and ensure_session_token to user model

--- MusicApp/app/models/user.rb
+++ MusicApp/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     validates, :email, :session_token, :password_digest, presence: true
     validates, :password, length: {minimum: 6, allow_nil: true}
-
+    after_initialize :ensure_session_token
+    
     attr_reader :password
 
     def self.find_by_credentials(email, password)
@@ -16,5 +17,15 @@ class User < ApplicationRecord
     def password=(password)
       @password = password
       self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def reset_session_token!
+      self.session_token = SecureRandom.urlsafe_base64
+      self.save!
+      self.session_token
+    end
+
+    def ensure_session_token
+      self.session_token ||= SecureRandom.urlsafe_base64
     end
 end
